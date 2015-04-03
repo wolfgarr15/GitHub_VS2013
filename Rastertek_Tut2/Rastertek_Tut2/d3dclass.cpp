0a7089eb6ef0574899bf889aaf047b919ab6a71f
wolfgarr15 <wolfgarr15@gmail.com>
2015-04-03 11:26:34 -0500

    Setup back buffer in D3DClass::Initialize(...).

--- Rastertek_Tut2/Rastertek_Tut2/d3dclass.cpp
+++ Rastertek_Tut2/Rastertek_Tut2/d3dclass.cpp
@@ -33,11 +33,14 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool vsync, HWND hw
 	IDXGIAdapter* adapter;
 	IDXGIOutput* adapterOutput;
 	UINT numModes;
+	UINT modeWidth;
+	UINT modeHeight;
 	UINT numerator;
 	UINT denominator;
-	UINT stringLength;
+	int error;
 	DXGI_MODE_DESC* displayModeList;
 	DXGI_ADAPTER_DESC adapterDesc;
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
 	D3D_FEATURE_LEVEL featureLevel;
 	ID3D11Texture2D* backBufferPtr;
 	D3D11_TEXTURE2D_DESC depthBufferDesc;
@@ -51,7 +54,87 @@ bool D3DClass::Initialize(int screenWidth, int screenHeight, bool vsync, HWND hw
 	m_vsync_enabled = vsync;
 	
 	// Create a DirectX graphics interface factory.
-	result = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);  // <--- What is this even doing!?
+	result = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);
 	if(FAILED(result))
 		return false;
+
+	// Use the factory to create an adapter for the graphics interface.
+	result = factory->EnumAdapters(0, &adapter);
+	if(FAILED(result))
+		return false;
+
+	// Enumerate the primary adapter output.
+	result = adapter->EnumOutputs(0, &adapterOutput);
+	if(FAILED(result))
+		return false;
+
+	// Get the number of display modes that fit the DXGI_FORMAT_R8G8B8A8_UNORM format.
+	result = adapterOutput->GetDisplayModeList(DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numModes, NULL);
+	if(FAILED(result))
+		return false;
+
+	// Create a list to hold all possible display modes.
+	displayModeList = new DXGI_MODE_DESC[numModes];
+	if(!displayModeList)
+		return false;
+
+	// Now fill the display mode list.
+	result = adapterOutput->GetDisplayModeList(DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numModes, displayModeList);
+	if(FAILED(result))
+		return false;
+
+	// Go through the list of supported display modes and select the one that matches the screen width and height.
+	for(UINT i = 0; i < numModes; i++)
+	{
+		modeWidth = displayModeList[i].Width;
+		modeHeight = displayModeList[i].Height;
+
+		if(modeWidth == (UINT)screenWidth && modeHeight == (UINT)screenHeight)
+		{
+			numerator = displayModeList[i].RefreshRate.Numerator;
+			denominator = displayModeList[i].RefreshRate.Denominator;
+		}
+	}
+
+	// Get the video grapgics adapter (VGA) description.
+	result = adapter->GetDesc(&adapterDesc);
+	if(FAILED(result))
+		return false;
+
+	// Store the dedicated VGA memory in megabytes.
+	m_videoCardMemory = (int)(adapterDesc.DedicatedVideoMemory / 1024 / 1024);
+
+	// Store the VGA name.
+	error = wcstombs_s(NULL, m_videoCardDescription, 128, adapterDesc.Description, 128);
+	if(error != 0)
+		return false;
+
+	// Release the display mode list.
+	delete [] displayModeList;
+	displayModeList = 0;
+
+	// Release the adapter output.
+	adapterOutput->Release();
+	adapterOutput = 0;
+
+	// Release the adapter.
+	adapter->Release();
+	adapter = 0;
+
+	// Release the factory.
+	factory->Release();
+	factory = 0;
+
+	// Initialize the swap chain description.
+	SecureZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
+
+	// Set to a single back buffer.
+	swapChainDesc.BufferCount = 1;
+
+	// Set the width and height of the back buffer.
+	swapChainDesc.BufferDesc.Width = screenWidth;
+	swapChainDesc.BufferDesc.Height = screenHeight;
+
+	// Set the back buffer to a regular 32-bit surface.
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 }
