c9850631da3ff2fa889efd4330dfdcf74ecd40f7
wolfgarr15 <wolfgarr15@gmail.com>
2015-04-13 16:54:26 -0500

    Meh...

---
+++ Selenium_Prac1/Selenium_Prac1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium_Prac1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ChromeDriver ChromeDriver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Navigate().GoToUrl("http://www.google.com/");
+            ChromeDriver.FindElementById("gbqfq").SendKeys("Google");
+            ChromeDriver.FindElementById("gbqfq").SendKeys(Keys.Enter);
+        }
+
+        [TestCleanup]
+
+        public void Shutdown()
+        {
+            ChromeDriver.Quit();
+        }
+    }
+}
