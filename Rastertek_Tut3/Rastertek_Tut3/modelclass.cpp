e2e71560a5a847fb899df513c8918520b4861851
wolfgarr15 <wolfgarr15@gmail.com>
2015-04-20 12:38:37 -0500

    Made progress on modelclass.cpp.

--- Rastertek_Tut3/Rastertek_Tut3/modelclass.cpp
+++ Rastertek_Tut3/Rastertek_Tut3/modelclass.cpp
@@ -1,11 +1,114 @@
+////////////////////////////////
+// Filename: modelclass.cpp
+////////////////////////////////
+
+////////////////////////////////
+// Class Header
+//------------------------------
 #include "modelclass.h"
 
-
+///////////////////////////////
+// Class Definition
+//-----------------------------
 ModelClass::ModelClass()
 {
+	m_vertexBuffer = 0;
+	m_indexBuffer = 0;
 }
 
+ModelClass::ModelClass(const ModelClass& src) {}
 
-ModelClass::~ModelClass()
+ModelClass::~ModelClass() {}
+
+bool ModelClass::Initialize(ID3D11Device* device)
 {
+	bool result;
+
+	// Initialize the vertex and index buffers.
+	result = InitializeBuffers(device);
+	if (!result)
+		return false;
+
+	return true;
+}
+
+void ModelClass::Shutdown()
+{
+	// Release the vertex and index buffers.
+	ShutdownBuffers();
+	return;
+}
+
+void ModelClass::Render(ID3D11DeviceContext* deviceContext)
+{
+	// Put the vertex and index buffers on the render pipeline.
+	RenderBuffers(deviceContext);
+	return;
+}
+
+int ModelClass::GetIndexCount()
+{
+	return m_indexCount;
+}
+
+bool ModelClass::InitializeBuffers(ID3D11Device* device)
+{
+	VertexType* vertices;
+	ULONG* indices;
+	D3D11_BUFFER_DESC vertexBufferDesc;
+	D3D11_BUFFER_DESC indexBufferDesc;
+	D3D11_SUBRESOURCE_DATA vertexData;
+	D3D11_SUBRESOURCE_DATA indexData;
+	HRESULT result;
+
+	// Set the number of vertices in the vertex array.
+	m_vertexCount = 3;
+
+	// Set the number of indices in the index array.
+	m_indexCount = 3;
+
+	// Create the vertex array.
+	vertices = new VertexType[m_vertexCount];
+	if (!vertices)
+		return false;
+
+	// Create the index array.
+	indices = new ULONG[m_indexCount];
+	if (!indices)
+		return false;
+
+	// Load the vertex array with data. REMEMBER: Vertices follow a clockwise order.
+	vertices[0].position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);  // Bottom left.
+	vertices[0].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	vertices[1].position = D3DXVECTOR3(0.0f, 1.0f, 0.0f);    // Top center.
+	vertices[1].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	vertices[2].position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);   // Bottom right.
+	vertices[2].color = D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	// Load the index array with data.
+	indices[0] = 0; // Bottom left.
+	indices[1] = 1; // Top center.
+	indices[2] = 2; // Bottom right.
+
+	// Setup the description of the static vertex buffer.
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof(VertexType) * m_vertexCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	// Give the subresource structure a pointer to the vertex data.
+	vertexData.pSysMem = vertices;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	// Now create the vertex buffer.
+	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer);
+	if (FAILED(result))
+		return false;
+
+	
 }
