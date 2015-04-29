41238d97f081eb7cce1cfd05d6db3c87e645dc5a
wolfgarr15 <wolfgarr15@gmail.com>
2015-04-29 16:53:38 -0500

    Started Tutorial 8... Many bugs to fix!

---
+++ Rastertek_Tut8/Rastertek_Tut8/lightclass.h
@@ -0,0 +1,44 @@
+////////////////////////////////
+// Filename: lightclass.h
+////////////////////////////////
+
+////////////////////////////////
+// Pre-processing Directives
+//-----------------------------
+#pragma once
+
+///////////////////////////////
+// Includes
+//-----------------------------
+#include <D3DX10math.h>
+
+///////////////////////////////
+// Class Declaration
+//-----------------------------
+class LightClass
+{
+public:
+	LightClass();
+	LightClass(const LightClass& src);
+	~LightClass();
+
+	void SetAmbientColor(float red, float green, float blue, float alpha);
+	void SetDiffuseColor(float red, float green, float blue, float alpha);
+	void SetDirection(float x, float y, float z);
+	void SetSpecularColor(float red, float green, float blue, float alpha);
+	void SetSpecularPower(float power);
+
+	D3DXVECTOR4 GetAmbientColor();
+	D3DXVECTOR4 GetDiffuseColor();
+	D3DXVECTOR3 GetDirection();
+	D3DXVECTOR4 GetSpecularColor();
+	float GetSpecularPower();
+
+private:
+	D3DXVECTOR4 m_ambientColor;
+	D3DXVECTOR4 m_diffuseColor;
+	D3DXVECTOR3 m_direction;
+	D3DXVECTOR4 m_specularColor;
+	float m_specularPower;
+};
+
