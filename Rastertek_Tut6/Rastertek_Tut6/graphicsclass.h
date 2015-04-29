611c36748d27d6fa43b86972418b72ac2d6b55fc
wolfgarr15 <wolfgarr15@gmail.com>
2015-04-29 09:15:10 -0500

    Began Tutorial 6.

---
+++ Rastertek_Tut6/Rastertek_Tut6/graphicsclass.h
@@ -0,0 +1,52 @@
+////////////////////////////////
+// Filename: graphicsclass.h
+////////////////////////////////
+
+////////////////////////////////
+// Pre-processing Directives
+//-----------------------------
+#pragma once
+
+///////////////////////////////
+// Includes
+//-----------------------------
+#include <fstream>
+#include "d3dclass.h"
+#include "cameraclass.h"
+#include "modelclass.h"
+#include "lightclass.h"
+#include "lightshaderclass.h"
+
+///////////////////////////////
+// Globals
+//-----------------------------
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+///////////////////////////////
+// Class Declaration
+//-----------------------------
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass& src);
+	~GraphicsClass();
+
+	bool Initialize(int screenWidth, int screenHeight, HWND hwnd);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render(float rotation);
+
+private:
+	D3DClass* m_D3D;
+	CameraClass* m_Camera;
+	ModelClass* m_Model;
+	LightClass* m_Light;
+	LightShaderClass* m_LightShader;
+};
+
